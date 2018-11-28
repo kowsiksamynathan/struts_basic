42dc1e64ac424156d79f403e4f7b1ec807a5f9e0
kowsiksamynathan <45425789+kowsiksamynathan@users.noreply.github.com>
2018-11-28 18:39:31 +0530

    Add files via upload

---
+++ struts-application/simple-war/WEB-INF/classes/class1.java
@@ -0,0 +1,1 @@
+import com.opensymphony.xwork2.ActionSupport;public class class1 extends ActionSupport{    public String execute() throws Exception {        System.out.println("kowsik");        return "success";    }}
---
+++ struts-application/simple-war/input1.jsp
@@ -0,0 +1,9 @@
+<html>
+<head>
+  <title>Hello World</title>
+</head>
+<body>
+  <h1>User Input Form</h1>
+ <p>welcome home</p>
+</body>
+</html>
